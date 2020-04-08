02aee443f1a3471a792f8f3b7f3fcd3f634cf7e8
corn-cloud <comowaiye@gmail.com>
2020-04-08 11:01:34 -0500

    enoughtime

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,56 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  constructor(){
+    super()
+      this.state ={
+        transactions: [],
+        displayTransactions: [],
+        // form: 
+        filter: ''
+      }
+    
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(res => res.json())
+    .then(transactions => {
+      this.setState({
+        transactions: transactions,
+        displayTransactions: transactions
+      })
+    })
+  }
+
+  newTransaction = (e) =>{
+    
+    e.preventDefault()
+    let addedTransaction ={
+      date: e.target[0].value,
+      description: e.target[1].value,
+      category: e.target[2].value,
+      amount: e.target[3].value
+      }
+      // console.log(addedTransaction)
+    this.setState({
+      displayTransactions: [...this.state.transactions, addedTransaction],
+
+    })
+  }
+
+  // filterTransactions(){
+    
+
+
+
+
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm newTransaction={this.newTransaction}/>
+        <TransactionsList transactions={this.state.displayTransactions}  />
       </div>
     );
   }
