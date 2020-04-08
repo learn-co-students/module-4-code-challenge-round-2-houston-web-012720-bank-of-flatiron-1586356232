fa1842de63001c88e7fc3782cc91b2996485d99e
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-04-08 10:59:18 -0500

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,48 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const URL = "http://localhost:6001/transactions"
+
 class AccountContainer extends Component {
+
+  
+
+  constructor(){
+    super()
+    this.state = {
+      transactions: [],
+      search: ""
+    }
+  }
+
+  componentDidMount() {fetch(URL).then(resp => resp.json()).then(transactions => this.setState({transactions}))}
+
+  changeSearch = (search) => this.setState({search})
+
+  filterTransactions = () => this.state.transactions.filter(tr => tr.description.toLowerCase().includes(this.state.search.toLowerCase()))
+  
+  submitTransaction = (transaction) => {
+    let params = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(transaction)
+    }
+    fetch(URL, params).then(resp => resp.json()).then(tr => this.setState({transactions: [...this.state.transactions, tr]}))
+  }
+
+  deleteTransaction = (id) => {
+    let params = {method: "DELETE"}
+    fetch(`${URL}/${id}`, params).then(this.setState({transactions: this.state.transactions.filter(tr => tr.id !== id)}))
+  }
+
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search changeSearch = {this.changeSearch}/>
+        <AddTransactionForm submitTransaction = {this.submitTransaction}/>
+        <div>Double Click to delete transaction</div>
+        <TransactionsList transactions = {this.filterTransactions()} deleteTransaction = {this.deleteTransaction} />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  changeState = (e) => this.setState({[e.target.name]: e.target.value})
+
+  submitForm = (e) => {
+    e.preventDefault()
+    return this.checkIfTransactionHasAllKeysAndIfNotShowWarning() ? this.props.submitTransaction(this.state) : false
+  }
+
+  checkIfTransactionHasAllKeysAndIfNotShowWarning = () => {
+    let s = this.state
+    if (s.hasOwnProperty('date')&&s.hasOwnProperty('description')&&s.hasOwnProperty('category')&&s.hasOwnProperty('amount')){
+      return true
+    }
+    else{
+      alert("All fields Required")
+      return false
+    }
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit = {this.submitForm}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input onChange = {this.changeState} type="date" name="date" />
+            <input onChange = {this.changeState} type="text" name="description" placeholder="Description" />
+            <input onChange = {this.changeState} type="text" name="category" placeholder="Category" />
             <input
+              onChange = {this.changeState}
               type="number"
               name="amount"
               placeholder="Amount"
