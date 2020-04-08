89c90aa2b97020362f7b698f2eaaa6280a30a0bf
vienpham2019 <vienpham2019@gmail.com>
2020-04-08 10:49:03 -0500

    done

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-const Transaction = () => {
+const Transaction = (props) => {
+  let trans = props.transaction
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{trans.date}</td>
+      <td>{trans.description}</td>
+      <td>{trans.category}</td>
+      <td>{trans.amount}</td>
+      <td>
+        <button onClick={() => props.removeTransaction(trans)}>
+          Delete
+        </button>
+      </td>
     </tr>
   );
 };
